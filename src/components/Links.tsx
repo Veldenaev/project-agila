f05f1322a971702892b63f570aaa536ebe3316c7
Daryll Ko <daryll.ko@gmail.com>
2024-02-25 19:37:18 +0800

    update: add `key` prop

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -17,8 +17,8 @@ export default function Links() {
       route: "/notification_management",
     },
   ];
-  const pagesJsx = pages.map(({name, route}) =>
-      <a href={route} className="w-full hover:bg-white/10"><div>{name} page</div></a>
+  const pagesJsx = pages.map(({name, route}, index) =>
+      <a key={index} href={route} className="w-full hover:bg-white/10"><div>{name} page</div></a>
     );
 
   return (<div className="w-[30%] border-r border-white h-full text-white flex flex-col items-center text-center">
