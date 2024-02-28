2c7e0c0d6bcf45a01fa9f1df35cf01308a4f3ca6
Daryll Ko <daryll.ko@gmail.com>
2024-02-29 00:39:21 +0800

    update: unify dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 
 import Links from "../components/Links";
+import Shadow from "../components/Shadow";
 
 export default function Dashboard() {
   return (
@@ -9,16 +10,19 @@ export default function Dashboard() {
         <title>Dashboard | Project Agila</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Shadow/>
       <main className="flex min-h-screen flex-col items-center bg-bookshelf bg-cover bg-center">
 
         <Links/>
 
-        <h1 className="block text-white font-sans text-center text-2xl pb-4 mt-4">Welcome, Client Name</h1>
+        <h1 className="mt-auto z-10 text-white font-sans text-center text-2xl mb-6 mt-4">Welcome, Client Name</h1>
 
-        <div id="dashboard" className="flex flex-row w-4/5 h-2/3 bg-white rounded-xl border-gray border-4 p-4 pt-0">
+        <div id="dashboard" className="mb-auto z-10 flex flex-row w-4/5 h-2/3 gap-5 bg-white rounded-xl border-gray border-4 p-4 pt-0">
 
-          <div id="transactions" className="flex flex-col w-1/2 h-full">
 
+          <div id="transactions" className="flex flex-col gap-2 w-1/2 h-full">
+
+            <div>
             <h1 className="block text-black font-sans text-center text-1xl p-1">Settled Transactions</h1>
 
             <div className="h-60 border-gray border-4 rounded-xl w-full">
@@ -32,7 +36,9 @@ export default function Dashboard() {
               </tbody>
             </table>
             </div>
+            </div>
 
+            <div>
             <h1 className="block text-black font-sans text-center text-1xl p-1">Unsettled Transactions</h1>
 
             <div className="h-60 border-gray border-4 rounded-xl w-full">
@@ -46,23 +52,28 @@ export default function Dashboard() {
               </tbody>
             </table>
             </div>
+            </div>
 
           </div>
 
-          <div id="notifications" className="flex flex-col w-1/2 h-full">
+          <div id="notifications" className="flex flex-col gap-2 w-1/2 h-full">
 
+            <div>
             <h1 className="block text-black font-sans text-center text-1xl p-1">Notifications For All Clients</h1>
 
             <div className="h-60 border-gray border-4 rounded-xl w-full">
             <table id="settled" className="w-full">
             </table>
             </div>
+            </div>
 
+            <div>
             <h1 className="block text-black font-sans text-center text-1xl p-1">Notifications For You</h1>
 
             <div className="h-60 border-gray border-4 rounded-xl w-full">
             <table id="unsettled" className="w-full">
             </table>
+            </div>
             </div>
 
           </div>
