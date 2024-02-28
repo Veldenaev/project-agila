32cbf943384d15e20c396c3de9d63944e5fec7b3
Daryll Ko <daryll.ko@gmail.com>
2024-02-29 00:25:52 +0800

    update: unify login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 
 import Links from "../components/Links";
+import Shadow from "../components/Shadow";
+
 import bgimg from "assets/bookshelfbg.jpg";
 
 export default function Login() {
@@ -11,16 +13,18 @@ export default function Login() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Shadow/>
+
       <main className="flex min-h-screen flex-col items-center bg-bookshelf bg-cover bg-center">
 
         <Links/>
 
-        <div className="flex flex-row items-center mt-40">
+        <div className="z-10 mt-auto mb-auto flex flex-row gap-16 items-center">
 
-          <div className="mr-16">
+          <div className="flex flex-col items-center gap-12">
 
-            <h1 className="block font-sans text-center text-7xl text-white">Agila Law Offices</h1>
-            <h2 className="block font-sans text-center text-5xl text-white">Be Agile.</h2>
+            <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[4rem]">Agila Law Offices</h1>
+            <h2 className="text-2xl tracking-tight text-white sm:text-[1.5rem]">Be Agile.</h2>
 
           </div>
 
@@ -31,13 +35,13 @@ export default function Login() {
             <form>
                 <div className="mb-4 font-sans">
                     <label className="block text-sm font-medium text-gray-700">username</label>
-                    <input type="email" id="email" name="email" className="mt-1 p-2 block w-full rounded-md border border-gray-300"></input>
+                    <input type="email" id="email" name="email" disabled className="mt-1 p-2 block w-full rounded-md border border-gray-300"></input>
                 </div>
                 <div className="mb-4">
                     <label className="block text-sm font-medium text-gray-700">password</label>
-                    <input type="password" id="password" name="password" className="mt-1 p-2 block w-full rounded-md border border-gray-300"></input>
+                    <input type="password" id="password" name="password" disabled className="mt-1 p-2 block w-full rounded-md border border-gray-300"></input>
                 </div>
-                <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded-md">Login</button>
+                <button type="submit" disabled className="w-full bg-blue-500 text-white p-2 rounded-md">Login</button>
             </form>
 
             <p className="font-sans block pt-4 text-center text-xs">Forgot password? Contact your system administrator: admin@lawoffice.com</p>
