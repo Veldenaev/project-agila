a30b0cd279df86764600eb4a13f4245cd0b91104
Daryll Ko <daryll.ko@gmail.com>
2024-02-28 14:44:46 +0800

    test: remove comments in `index.spec.ts`

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -2,17 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Project Agila/);
 });
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://localhost:3000/');
-// 
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-// 
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
