37bfd9adfa50186213b8b214e4f20a83e4f97508
Daryll Ko <daryll.ko@gmail.com>
2024-02-29 09:51:01 +0800

    add: use "real" data for notif management page

--- src/pages/notification_management.tsx
+++ src/pages/notification_management.tsx
@@ -8,27 +8,27 @@ export default function NotificationManagement() {
   const notifications = [
     {
       id: 1,
-      notificationTitle: "Notification 1",
-      details: "Detail 1",
-      lastUpdated: "2024-02-28",
-      viewers: "Client 1",
-      creator: "Attorney 1",
+      notificationTitle: "Lorem ipsum...",
+      details: "Ipsum lorem...",
+      lastUpdated: "2018-01-01",
+      viewers: "Maya Fey, Miles Edgeworth",
+      creator: "Admin",
     },
     {
       id: 2,
-      notificationTitle: "Notification 2",
-      details: "Detail 2",
-      lastUpdated: "2024-02-27",
-      viewers: "Client 2",
-      creator: "Attorney 2",
+      notificationTitle: "Not again...",
+      details: "What did you...",
+      lastUpdated: "2017-12-31",
+      viewers: "Maya Fey",
+      creator: "Phoenix Wright",
     },
     {
       id: 3,
-      notificationTitle: "Notification 3",
-      details: "Detail 3",
-      lastUpdated: "2024-02-26",
-      viewers: "Client 3",
-      creator: "Attorney 3",
+      notificationTitle: "Buy me a coffee",
+      details: "I'm busy reading an autopsy report, so can you...",
+      lastUpdated: "2017-12-30",
+      viewers: "Detective Gumshoe",
+      creator: "Miles Edgeworth",
     },
   ];
 
@@ -43,7 +43,7 @@ export default function NotificationManagement() {
         <Links />
         <table className="border-gray z-10 mb-auto mt-auto border-4 bg-white p-4">
           <caption className="z-10 mb-6 mt-4 mt-auto text-center font-sans text-2xl text-white">
-            Notifications
+            Notifications <span className="font-bold text-[hsl(280,100%,80%)]">(admin)</span>
           </caption>
           <thead>
             <tr className="bg-gray-800 text-white">
@@ -59,19 +59,19 @@ export default function NotificationManagement() {
             {notifications.map((notification) => (
               <tr key={notification.id}>
                 <td className="px-4 py-2">{notification.notificationTitle}</td>
-                <td className="px-4 py-2">{notification.details}</td>
+                <td className="px-4 py-2 max-w-56">{notification.details}</td>
                 <td className="px-4 py-2">{notification.lastUpdated}</td>
                 <td className="px-4 py-2">{notification.viewers}</td>
                 <td className="px-4 py-2">{notification.creator}</td>
-                <td className="px-4 py-2">
-                  <button className="mr-2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+                <td className="px-4 py-2 flex gap-2">
+                  {/*<button className="mr-2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
                     Add
-                  </button>
-                  <button className="mr-2 rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700">
-                    Delete
-                  </button>
+                  </button>*/}
                   <button className="rounded bg-yellow-500 px-4 py-2 font-bold text-white hover:bg-yellow-700">
                     Edit
+                  </button>
+                  <button className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700">
+                    Delete
                   </button>
                 </td>
               </tr>
