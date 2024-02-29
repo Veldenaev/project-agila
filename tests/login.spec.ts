abd331c73e2570e8550efbafa5828af3150ae601
Daryll Ko <daryll.ko@gmail.com>
2024-02-29 15:16:49 +0800

    test: add tests for Login page login button

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -4,3 +4,13 @@ test("has 'Login' in title", async ({ page }) => {
   await page.goto("/login");
   await expect(page).toHaveTitle(/Login/);
 });
+
+test("has 'Login' button", async ({ page }) => {
+  await page.goto("/login");
+  await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+})
+
+test("'Login' button is disabled", async ({ page }) => {
+  await page.goto("/login");
+  await expect(page.getByRole('button', { name: 'Login' })).toBeDisabled();
+})
