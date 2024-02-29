2746fca97266ecfed09d7de2c6785d67c3546eff
Daryll Ko <daryll.ko@gmail.com>
2024-02-29 15:10:32 +0800

    test: add subtitle check for landing page

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -4,3 +4,8 @@ test("has title 'Project Agila'", async ({ page }) => {
   await page.goto("/");
   await expect(page).toHaveTitle(/^Project Agila$/);
 });
+
+test("has subtitle", async ({ page }) => {
+  await page.goto("/");
+  await expect(page.getByText("Empowering transactions between clients and lawyers.")).toBeVisible();
+})
