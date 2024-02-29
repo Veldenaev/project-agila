24fcf53f0438c3f22f466de32694ca32c40ccba6
Daryll Ko <daryll.ko@gmail.com>
2024-02-29 09:39:08 +0800

    update: make background shadow slightly darker

--- src/components/Shadow.tsx
+++ src/components/Shadow.tsx
@@ -1,5 +1,5 @@
 export default function Shadow() {
   return (
-    <div className="inset absolute h-full w-full bg-black bg-opacity-60"></div>
+    <div className="inset absolute h-full w-full bg-black bg-opacity-70"></div>
   );
 }
