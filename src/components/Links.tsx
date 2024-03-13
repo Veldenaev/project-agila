fab0679ba43bafc26f2d04155ef0146241587bb8
Daryll Ko <daryll.ko@gmail.com>
2024-03-13 20:35:17 +0800

    add: include dynamic routes, initial pages

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,31 +1,54 @@
+import { useRouter } from "next/router";
+
 export default function Links() {
+  const router = useRouter();
   const pages = [
     {
       name: "Landing",
       route: "/",
     },
+    //    {
+    //      name: "Login",
+    //      route: "/login",
+    //    },
+    //    {
+    //      name: "Dashboard",
+    //      route: "/dashboard",
+    //    },
+    //    {
+    //      name: "Transaction management",
+    //      route: "/transaction_management",
+    //    },
+    //    {
+    //      name: "Notification management",
+    //      route: "/notification_management",
+    //    },
     {
-      name: "Login",
-      route: "/login",
+      name: "Accounts (L)",
+      route: "/accounts/phoenix",
     },
     {
-      name: "Dashboard",
-      route: "/dashboard",
+      name: "Accounts (M)",
+      route: "/accounts/gumshoe",
     },
     {
-      name: "Transaction management",
-      route: "/transaction_management",
+      name: "Cases (L)",
+      route: "/cases/phoenix",
     },
     {
-      name: "Notification management",
-      route: "/notification_management",
+      name: "Cases (M)",
+      route: "/cases/gumshoe",
+    },
+    {
+      name: "Case Overview (L)",
+      route: "/overviews/bridge_to_the_turnabout",
     },
   ];
   const pagesJsx = pages.map(({ name, route }, index) => (
     <a
       key={index}
       href={route}
-      className="z-10 flex h-full w-full items-center justify-center bg-violet-500 hover:bg-violet-500/80"
+      className={`z-10 flex h-full w-full items-center justify-center hover:bg-violet-500/80 ${router.asPath === route ? "bg-violet-500/80" : "bg-violet-500"}`}
     >
       <div>{name} page</div>
     </a>
