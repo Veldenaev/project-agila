bde512083f13a05504c7979948b21f1063cdbabf
Daryll Ko <daryll.ko@gmail.com>
2024-03-14 22:44:22 +0800

    update: finalize accounts page

--- src/pages/accounts/[account].tsx
+++ src/pages/accounts/[account].tsx
@@ -5,117 +5,59 @@ import { useRouter } from "next/router";
 import Links from "../../components/Links";
 import Shadow from "../../components/Shadow";
 
+import {
+  allClients,
+  allCases,
+  allLawyers,
+  allCaseAssignments,
+} from "../../utils/data";
+import type { Client, Lawyer } from "../../utils/data";
+
 export default function Accounts() {
   const router = useRouter();
-  // Placeholder Data
-  const Client = [
-    {
-      id: 1,
-      LastName: "Smith",
-      FirstName: "Lauren",
-      MiddleName: "Regalia",
-      Email: "slr@email.com",
-      CellNum: "09170002222",
-      TelNum: "000-0000",
-      CityAdd: "Address 1",
-      Remarks: "Remarks 1",
-      Case: "turnabout",
-    },
-    {
-      id: 2,
-      LastName: "Arele",
-      FirstName: "Polius",
-      MiddleName: "Garate",
-      Email: "apg@email.com",
-      CellNum: "09170001111",
-      TelNum: "000-0001",
-      CityAdd: "Address 2",
-      Remarks: "Remarks 2",
-      Case: "turnabout",
-    },
-    {
-      id: 2,
-      LastName: "Arele",
-      FirstName: "Polius",
-      MiddleName: "Garate",
-      Email: "apg@email.com",
-      CellNum: "09170001111",
-      TelNum: "000-0001",
-      CityAdd: "Address 2",
-      Remarks: "Remarks 2",
-      Case: "turnabout",
-    },
-    {
-      id: 2,
-      LastName: "Arele",
-      FirstName: "Polius",
-      MiddleName: "Garate",
-      Email: "apg@email.com",
-      CellNum: "09170001111",
-      TelNum: "000-0001",
-      CityAdd: "Address 2",
-      Remarks: "Remarks 2",
-      Case: "turnabout",
-    },
-    {
-      id: 2,
-      LastName: "Arele",
-      FirstName: "Polius",
-      MiddleName: "Garate",
-      Email: "apg@email.com",
-      CellNum: "09170001111",
-      TelNum: "000-0001",
-      CityAdd: "Address 2",
-      Remarks: "Remarks 2",
-      Case: "turnabout",
-    },
-  ];
-  const Lawyer = [
-    {
-      id: 1,
-      LastName: "Sophia",
-      FirstName: "John",
-      MiddleName: "Johnson",
-      Email: "sjj@email.com",
-      CellNum: "09170003333",
-      TelNum: "000-0002",
-      CityAdd: "Address 3",
-      Case: "turnabout",
-    },
-    {
-      id: 2,
-      LastName: "Brown",
-      FirstName: "Alexander",
-      MiddleName: "Grace",
-      Email: "bag@email.com",
-      CellNum: "09170004444",
-      TelNum: "000-0003",
-      CityAdd: "Address 4",
-      Case: "turnabout",
-    },
-  ];
-  const title = `${router.query.account ?? "<>"}'s accounts | Project Agila`;
+  const currLawyer: Lawyer =
+    allLawyers.filter(
+      (lawyer) => lawyer.lawyerId.toString() === router.query.account,
+    )[0] ?? allLawyers[0]!;
+  const toShow: Client[] = allClients;
+  // const toShow = allCaseAssignments
+  //   .filter(
+  //     (caseAssignment) =>
+  //       caseAssignment.lawyerId === router.query.account.toString(),
+  //   )
+  //   .map((caseAssignment) =>
+  //     allCases.filter((c) => c.caseNum == caseAssignment.caseNum),
+  //   );
   return (
     <>
       <Head>
-        <title>{title}</title>
+        <title>Accounts | Project Agila</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
       <Shadow />
       <main className="flex min-h-screen flex-col items-center">
         <Links />
-        <h1 className="z-10 mb-6 mt-4 mt-auto text-center font-sans text-2xl text-white">
-          <span className="font-bold text-[hsl(280,100%,80%)]">
-            {router.query.account}
-          </span>
-          &apos;s accounts
+        <h1 className="z-10 mb-6 mt-auto text-center font-sans text-2xl text-white">
+          {currLawyer.isManager ? (
+            <>
+              Managing accounts as{" "}
+              <span className="font-bold text-[hsl(280,100%,80%)]">
+                {currLawyer.firstName} {currLawyer.lastName}
+              </span>
+            </>
+          ) : (
+            <>
+              <span className="font-bold text-[hsl(280,100%,80%)]">
+                {currLawyer.firstName} {currLawyer.lastName}
+              </span>
+              &apos;s accounts
+            </>
+          )}
         </h1>
-        <table className="border-gray z-10 mb-auto mt-auto border-4 bg-white p-4">
+        <table className="border-gray z-10 mb-auto border-4 bg-white p-4">
           <thead>
             <tr className="bg-gray-800 text-white">
-              <th className="px-4 py-2">Last Name</th>
-              <th className="px-4 py-2">First Name</th>
-              <th className="px-4 py-2">Middle Name</th>
+              <th className="px-4 py-2">Name</th>
               <th className="px-4 py-2">Email</th>
               <th className="px-4 py-2">Cellphone Number</th>
               <th className="px-4 py-2">Telephone Number</th>
@@ -125,26 +67,40 @@ export default function Accounts() {
             </tr>
           </thead>
           <tbody>
-            {Client.map((client) => (
-              <tr key={client.id}>
-                <td className="px-4 py-2">{client.LastName}</td>
-                <td className="px-4 py-2">{client.FirstName}</td>
-                <td className="px-4 py-2">{client.MiddleName}</td>
-                <td className="px-4 py-2">{client.Email}</td>
-                <td className="px-4 py-2">{client.CellNum}</td>
-                <td className="px-4 py-2">{client.TelNum}</td>
-                <td className="px-4 py-2">{client.CityAdd}</td>
-                <td className="px-4 py-2">{client.Remarks}</td>
-                <td className="flex gap-2 px-4 py-2">
-                  {/*<button className="mr-2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
-                    Add
-                  </button>*/}
-                  <Link href={`/cases/${client.Case}`}>
-                    <button className="rounded bg-blue-500 px-3 py-1 text-white hover:bg-blue-700"></button>
-                  </Link>
-                </td>
-              </tr>
-            ))}
+            {toShow.map(
+              (client, index) =>
+                (currLawyer.isManager || index < 2) && (
+                  <tr key={client.clientId}>
+                    <td className="px-4 py-2">
+                      {client.firstName} {client.lastName}
+                    </td>
+                    <td className="px-4 py-2">{client.email}</td>
+                    <td className="px-4 py-2">{client.cellNum}</td>
+                    <td className="px-4 py-2">{client.telNum}</td>
+                    <td className="px-4 py-2">{client.cityAdd}</td>
+                    <td className="px-4 py-2">{client.remarks}</td>
+                    <td className="flex gap-2 px-4 py-2">
+                      <Link
+                        href={`/cases/${currLawyer.isManager ? `c_${client.clientId}` : `l_${currLawyer.lawyerId}`}`}
+                      >
+                        <button className="rounded bg-blue-500 px-3 py-1 text-white hover:bg-blue-700">
+                          View cases
+                        </button>
+                      </Link>
+                      {currLawyer.isManager && (
+                        <>
+                          <button className="rounded bg-yellow-500 px-3 py-1 text-white hover:bg-yellow-700">
+                            Edit
+                          </button>
+                          <button className="rounded bg-red-500 px-3 py-1 text-white hover:bg-red-700">
+                            Delete
+                          </button>
+                        </>
+                      )}
+                    </td>
+                  </tr>
+                ),
+            )}
           </tbody>
         </table>
       </main>
