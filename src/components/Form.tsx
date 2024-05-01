d3e847a9ff087d3837b50e2966f80b858ce43105
Daryll Ko <daryll.ko@gmail.com>
2024-05-01 23:16:01 +0800

    update: organize `Form` code

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,9 +5,10 @@ interface Props<T extends object> {
   obj: T;
   type: string;
   name: string;
-  p_keys: string[];
+  keys: string[];
   hide: string[];
-  id_func: (t: T) => string | number;
+  textarea: string[];
+  identifier: (t: T) => string | number;
   adding: boolean;
   stay: boolean;
 }
@@ -16,9 +17,10 @@ export default function Form<T extends object>({
   obj,
   type,
   name,
-  p_keys,
+  keys,
   hide,
-  id_func,
+  textarea,
+  identifier,
   adding,
   stay,
 }: Props<T>) {
@@ -29,7 +31,7 @@ export default function Form<T extends object>({
   const submitData = async (e: React.SyntheticEvent) => {
     e.preventDefault();
     try {
-      await fetch(adding ? `/api/${type}` : `/api/${type}/${id_func(obj)}`, {
+      await fetch(adding ? `/api/${type}` : `/api/${type}/${identifier(obj)}`, {
         method: adding ? "POST" : "PUT",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newObj),
@@ -50,7 +52,7 @@ export default function Form<T extends object>({
       className="z-10 mx-auto my-auto flex flex-col gap-4"
     >
       <h1 className="text-center font-bold tracking-tight text-white sm:text-[2rem]">
-        <span className="text-agila">{name}</span> #{id_func(obj)}
+        <span className="text-agila">{name}</span> #{identifier(obj)}
       </h1>
       <div className="grid grid-cols-2 gap-3 rounded-lg bg-white p-4 pb-5">
         {Object.entries(newObj)
@@ -58,29 +60,50 @@ export default function Form<T extends object>({
           .map(([k, v], index) => (
             <div key={index} className="flex flex-col">
               <span className="pl-1 text-black">{k}</span>
-              <input
-                className="rounded-md border-2 border-solid border-black px-1 disabled:bg-gray-200"
-                autoFocus
-                onChange={(e) =>
-                  setNewObj((oldObj) => ({
-                    ...oldObj,
-                    [k]: e.target.value,
-                  }))
-                }
-                disabled={p_keys.includes(k) || !updating}
-                placeholder={k}
-                defaultValue={v as string}
-                value={v as string}
-                type={
-                  k.includes("Email")
-                    ? "email"
-                    : k === "Date"
-                      ? "date"
-                      : typeof v === "string"
-                        ? "text"
-                        : "number"
-                }
-              />
+
+              {textarea.includes(k) ? (
+                <textarea
+                  className="rounded-md border-2 border-solid border-black px-1 disabled:bg-gray-200"
+                  disabled={!updating}
+                  defaultValue={v as string}
+                  onChange={(e) =>
+                    setNewObj((oldObj) => ({
+                      ...oldObj,
+                      Status: e.target.value,
+                    }))
+                  }
+                ></textarea>
+              ) : (
+                <input
+                  className={`rounded-md ${k.match(/file/i) ? "" : "border-2 border-solid border-black disabled:bg-gray-200"} px-1`}
+                  autoFocus
+                  onChange={(e) =>
+                    setNewObj((oldObj) => ({
+                      ...oldObj,
+                      [k]:
+                        typeof v === "number"
+                          ? parseInt(e.target.value)
+                          : e.target.value,
+                    }))
+                  }
+                  min={0}
+                  disabled={keys.includes(k) || !updating}
+                  placeholder={k}
+                  defaultValue={k.match(/file/i) ? "" : (v as string)}
+                  value={k.match(/file/i) ? "" : (v as string)}
+                  type={
+                    k.match(/file/i)
+                      ? "file"
+                      : k.match(/email/i)
+                        ? "email"
+                        : k.match(/date/i)
+                          ? "date"
+                          : typeof v === "string"
+                            ? "text"
+                            : "number"
+                  }
+                />
+              )}
             </div>
           ))}
       </div>
