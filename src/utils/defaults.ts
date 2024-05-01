1319c4360c0739744979c795b1118ea7c830223d
Daryll Ko <daryll.ko@gmail.com>
2024-05-01 20:03:03 +0800

    add: include defaults for other types

--- src/utils/defaults.ts
+++ src/utils/defaults.ts
@@ -1,4 +1,9 @@
-import { type Case } from "@prisma/client";
+import {
+  type Payment,
+  type Case,
+  type Lawyer,
+  type Client,
+} from "@prisma/client";
 
 export const defaultCase: Case = {
   CaseNum: "",
@@ -8,4 +13,41 @@ export const defaultCase: Case = {
   Type: "",
   Title: "",
   Venue: "",
+};
+
+export const defaultClient: Client = {
+  ClientID: 0,
+  ContractID: 0,
+  FirstName: "",
+  LastName: "",
+  MiddleName: "",
+  Email: "",
+  CellNum: "",
+  TelNum: "",
+  CityAdd: "",
+  Remarks: "",
+  user: "",
+  pass: "",
+};
+
+export const defaultLawyer: Lawyer = {
+  LawyerID: 0,
+  FirstName: "",
+  LastName: "",
+  MiddleName: "",
+  Email: "",
+  AltEmail: "",
+  CellNum: "",
+  TelNum: "",
+  CityAdd: "",
+  isManager: false,
+  user: "",
+  pass: "",
+};
+
+export const defaultPayment: Payment = {
+  PaymentID: 0,
+  ClientID: 0,
+  Amount: 0,
+  Date: new Date(),
 };
