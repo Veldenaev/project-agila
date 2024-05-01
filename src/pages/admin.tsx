6278f8eb564c1980a0b4b0e3f6b7e27412b2c12d
Daryll Ko <daryll.ko@gmail.com>
2024-05-01 12:41:08 +0800

    update: make button text black

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -9,14 +9,14 @@ export default function Admin() {
           <div className="z-10 mb-auto mt-auto flex flex-row items-center gap-16">
             <div className="flex flex-col gap-5 text-center">
               <Link
-                className="w-full rounded-md bg-agila p-2 text-white transition-all hover:scale-105"
+                className="w-full rounded-md bg-agila p-2 text-black transition-all hover:scale-105"
                 href="/lawyer/all"
               >
                 Manage Staff
               </Link>
               <Link
                 href="/client/all"
-                className="w-full rounded-md bg-agila p-2 text-white transition-all hover:scale-105"
+                className="w-full rounded-md bg-agila p-2 text-black transition-all hover:scale-105"
               >
                 Manage Clients and Cases
               </Link>
