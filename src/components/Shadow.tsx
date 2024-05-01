72e583d8088dd47b5c73cfd9261deef283ade9c4
Daryll Ko <daryll.ko@gmail.com>
2024-05-01 14:18:12 +0800

    add: include login/logout button to `Shadow`

--- src/components/Shadow.tsx
+++ src/components/Shadow.tsx
@@ -1,18 +1,26 @@
 import Image from "next/image";
 import bookshelfbg from "../../assets/bookshelfbg.jpg";
 import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Shadow() {
+  const { data: session } = useSession();
   return (
     <>
       <div className="inset absolute z-10 h-full w-full bg-black bg-opacity-60" />
       <Image src={bookshelfbg} fill alt="Bookshelf background image" />
-      <Link
-        className="absolute left-5 top-5 z-20 text-white shadow-md hover:text-agila"
-        href="/"
-      >
-        Project Agila
-      </Link>
+      <div className="absolute z-20 flex w-full flex-row items-center justify-between p-5 text-white shadow-md">
+        <Link href="/">Project Agila</Link>
+        {session ? (
+          <button className="btn-blue" onClick={() => signIn()}>
+            Logout {session.user.id}
+          </button>
+        ) : (
+          <button className="btn-blue" onClick={() => signOut()}>
+            Login
+          </button>
+        )}
+      </div>
     </>
   );
 }
